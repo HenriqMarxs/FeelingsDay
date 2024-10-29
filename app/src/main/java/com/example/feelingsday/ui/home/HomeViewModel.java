a8cb7feefa124d2e7f69d0af131d6419efcaaf6a
henriqmarxs <marquesx19@gmail.com>
2024-10-29 02:08:00 -0300

    First Commit

---
+++ app/src/main/java/com/example/feelingsday/ui/home/HomeViewModel.java
@@ -0,0 +1,25 @@
+package com.example.feelingsday.ui.home;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import com.example.feelingsday.R;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Next Goals");
+
+    }
+
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
